fbef59550826a7c07001d9017d0d1b8b05701c28
baleato <baleato@gmail.com>
2015-03-07 20:17:44 +0100

    Added calendar iframe

--- scripts/main.js
+++ scripts/main.js
@@ -25,4 +25,13 @@ define('main', ['lamps'], function(lamps){
     attach(['#hotspot-2611337', '#hotspot-2612230'],
         'click', lamps.turnOff);
 
+    // Too hacky:
+    setTimeout(function(){
+        var target = document.getElementById('2590764').querySelector('.box-inner'),
+            iframeHtml = '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=flickeracc15%40gmail.com&amp;color=%23FFFFFF&amp;ctz=Europe%2FAmsterdam" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>';
+
+        target.firstElementChild.style.display = "none";
+        target.innerHTML = iframeHtml + target.innerHTML;
+    }, 1000)
+
 });
