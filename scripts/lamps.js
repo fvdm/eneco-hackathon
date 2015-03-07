9558771491af72b315e63bce7eea2aa2b14551b8
baleato <baleato@gmail.com>
2015-03-07 20:43:49 +0100

    Remove debugger command

--- scripts/lamps.js
+++ scripts/lamps.js
@@ -3,17 +3,13 @@ define('lamps', ['eneco/toon', 'devices/lamp'], function(toon, Lamp){
 
     var lamps = [];
 
-
     toon.lamps.list().then(
         function(response){
-            debugger;
             lamps = response;
-
-            console.log('list: ', arguments);
         },
         function(){
-            debugger;
-            // Hacky: default lamp if fails to get all of them
+            // Hacky: but as a back-up plan: default lamp if the call fails to get all lamps
+            // this call got stuck once, so I don't trust it!!!!
             lamps.push(new Lamp('b2-haet-0483:happ_smartplug_644EAC423D7'));
         });
 
