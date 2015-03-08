9875d2c3526a17321cda697a63b7ea27c15284d2
baleato <baleato@gmail.com>
2015-03-08 02:05:27 +0100

    User first calendar in the iframe

--- scripts/main.js
+++ scripts/main.js
@@ -80,11 +80,16 @@ define('main',
      */
     // Too hacky:
     setTimeout(function(){
-        var target = document.getElementById('2590764').querySelector('.box-inner'),
-            iframeHtml = '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=flickeracc15%40gmail.com&amp;color=%23FFFFFF&amp;ctz=Europe%2FAmsterdam" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>';
+        gCal.getCalendars().then(function(calendars){
+            // Show events for the first calendar
+            // TODO: avoid iframe, create content
+            var calendarId = calendars[0].id;
+            var target = document.getElementById('2590764').querySelector('.box-inner'),
+                iframeHtml = '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=' + calendarId + '&amp;color=%23FFFFFF&amp;ctz=Europe%2FAmsterdam" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>';
 
-        target.firstElementChild.style.display = "none";
-        target.innerHTML = iframeHtml + target.innerHTML;
+            target.firstElementChild.style.display = "none";
+            target.innerHTML = iframeHtml + target.innerHTML;
+        });
     }, 1000)
 
 });
